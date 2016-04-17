9fff86edda4cd0a08a26f63938d7459c42f9b9d0
Graham Mosley <gmosley@seas.upenn.edu>
2016-04-17 15:56:01 -0400

    removed tabs and added vote stub

--- server/popop.py
+++ server/popop.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/vote", methods=['GET', 'POST'])
+def vote():
+    if request.method == 'POST':
+        # record the vote
+        pass
+        
+    # get a job from DB
+    # return a rendered template with images
+    abort(418)
 
 if __name__ == "__main__":
     app.run(debug=True)
