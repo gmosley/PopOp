fd7f164c85dc50ea8b803265f9c9fe19f0226939
David Cao <davidcao@seas.upenn.edu>
2016-04-18 22:10:42 -0400

    made database inside server/, also fuck dylan

--- server/database.py
+++ server/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
 
-engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+# engine = create_engine('sqlite:////tmp/test.db', convert_unicode=True)
+engine = create_engine('sqlite:///./db/database.db', convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
 
 Base = declarative_base()
