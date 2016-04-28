22d3076b141758372a55cf4fdee22408d2b2a9bd
jchangc <jchangc@seas.upenn.edu>
2016-04-28 16:47:01 -0400

    Update exporter.py
    exporter from stackoverflow

--- data/aggregation/exporter.py
+++ data/aggregation/exporter.py
@@ -1,6 +1,11 @@
-import sys
-sys.path.append('../../server')
+import csv
 import database
 from models import ImageSet, Image, Job, Result, User
 
-def export_results:
+outfile = open('results.csv', 'wb')
+outcsv = csv.writer(outfile)
+records = session.query(ImageSet).all()
+[outcsv.writerow([getattr(curr, column.name) for column in ImageSet.__mapper__.columns]) for curr in records]
+# or maybe use outcsv.writerows(records)
+
+outfile.close()
