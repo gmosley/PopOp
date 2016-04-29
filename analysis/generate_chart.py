e049726a34240f856f0f9dec1d909808758a26fb
Graham Mosley <gmosley@seas.upenn.edu>
2016-04-28 22:43:26 -0400

    added generate_chart.py docs

--- analysis/generate_chart.py
+++ analysis/generate_chart.py
@@ -1,14 +1,19 @@
+"""
+This code uses final_rankings.csv to generate a HTML chart of the ranked images.
+jinja2 templating is used to make this easy to implement in server.py
+"""
+
 import jinja2
 import csv
 import json
 import operator
-
 
 env = jinja2.Environment(loader = jinja2.FileSystemLoader('.'))
 template = env.get_template('chart_template.html')
 
 data = []
 
+# read and parse final_rankings.csv
 with open('final_rankings.csv', 'rb') as csvfile:
     reader = csv.DictReader(csvfile)
 
@@ -16,12 +21,10 @@ with open('final_rankings.csv', 'rb') as csvfile:
         rankings = {}
         for (_, img_url, score) in json.loads(row['result']):
             rankings[img_url] = score
+        # order images by page rank score
         sorted_rankings = sorted(rankings.items(), key=operator.itemgetter(1), reverse=True)
         data.append({ 'description': row['description'], 'images': sorted_rankings})
 
 
 with open('aggregate_chart.html', 'w') as out:
     out.write(template.render(data=data))
-
-# >>> template = Template('Hello {{ name }}!')
-# >>> template.render(name='John Doe')
